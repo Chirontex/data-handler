1147971031baaf52f6eb3a491e87837d70e108b0
Dmitry Shumilin <infernozzz@yandex.ru>
2021-01-13 16:11:13 +0300

    0.01: initial

---
+++ autoloader.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *    Data Handler
+ *    Copyright (C) 2020  Dmitry Shumilin (dr.noisier@yandex.ru)
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+spl_autoload_register(function($classname) {
+
+    if (strpos($classname, 'DRNoisier\\DataHandler') !== false) {
+
+        $path = __DIR__.'/classes/';
+
+        $file = explode('\\', $classname);
+        $file = $file[count($file) - 1].'.php';
+
+        if (file_exists($path.$file)) require_once $path.$file;
+
+    }
+
+});
