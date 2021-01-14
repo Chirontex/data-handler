2e3adaddf47e42020b57e94d92321c46e00aa38e
Dmitry Shumilin <infernozzz@yandex.ru>
2021-01-14 16:09:37 +0300

    0.07: update dicteditor

--- dictedit.php
+++ dictedit.php
@@ -23,42 +23,63 @@ require_once __DIR__.'/autoloader.php';
 
 try {
 
+    if (!empty($filename)) $editor = new DictionaryEditor(__DIR__.'/dictionaries', $filename);
+
     switch ($command) {
 
         case 'set':
-            if (empty($key) || empty ($value)) {
 
-                echo 'Key or value not passed.';
-                die;
+            if (empty($filename)) echo 'Filename not passed.';
+            else {
 
-            } else $editor = new DictionaryEditor(__DIR__.'/dictionaries', $filename);
+                if (empty($key) || empty($value)) echo 'Key or value not passed.';
+                else {
 
-            if ($editor->set($key, $value)) echo 'Value saved to dictionary.';
-            else echo 'Value setting to dictionary failed: file not saved.';
+                    if ($editor->set($key, $value)) echo 'Value saved to dictionary.';
+                    else echo 'Value setting to dictionary failed: file not saved.';
+
+                }
+
+            }
+
             break;
 
         case 'view':
-            if (empty($key)) {
 
-                echo 'Key not passed.';
-                die;
+            if (empty($filename)) echo 'Filename not passed.';
+            else {
 
-            } else $editor = new DictionaryEditor(__DIR__.'/dictionaries', $filename);
+                if (empty($key)) echo 'Key not passed.';
+                else {
 
-            $view_result = $editor->view($key);
+                    $view_result = $editor->view($key);
 
-            if ($view_result === NULL) echo '(empty)';
-            else echo $view_result;
+                    if ($view_result === NULL) echo '(empty)';
+                    else echo $view_result;
+
+                }
+
+            }
+
             break;
 
         case 'help':
-            echo 'set — Set new value to dictionary. If dictionary not exist, it will be created.'."\n";
-            echo 'view — View dictionary value by the key.';
+
+            echo "\n";
+            echo "\t".'set -filename -key -value'."\n";
+            echo 'Set new value to dictionary. If dictionary not exist, it will be created.'."\n";
+            echo "\n";
+            echo "\t".'view -filename -key'."\n";
+            echo 'View dictionary value by the key.'."\n";
+
             break;
         
         default:
+
             echo 'Invalid command. Use "help" to learn available commands.';
+
             break;
+
     }
 
 } catch (DictionaryEditorException $e) {
